df5bf58d50093325b3d4735115774fe53d8c7a08
David Wildasin <dragonmaus@posteo.net>
2020-12-30 14:52:34 -0500

    Update dependencies

--- src/bin/purge.rs
+++ src/bin/purge.rs
@@ -2,16 +2,16 @@ use getopt::Opt;
 
 program::main!("purge");
 
-fn usage_line() -> String {
-    format!("Usage: {} [-h] path [path ...]", program::name("purge"))
+fn usage_line(program_name: &str) -> String {
+    format!("Usage: {} [-h] path [path ...]", program_name)
 }
 
-fn print_usage() {
-    println!("{}", usage_line());
+fn print_usage(program_name: &str) {
+    println!("{}", usage_line(program_name));
     println!("  -h   display this help");
 }
 
-fn program() -> program::Result {
+fn program(name: &str) -> program::Result {
     let mut args = program::args();
     let mut opts = getopt::Parser::new(&args, "h");
 
@@ -20,7 +20,7 @@ fn program() -> program::Result {
             None => break,
             Some(opt) => match opt {
                 Opt('h', None) => {
-                    print_usage();
+                    print_usage(name);
                     return Ok(0);
                 }
                 _ => unreachable!(),
@@ -30,7 +30,7 @@ fn program() -> program::Result {
 
     let args = args.split_off(opts.index());
     if args.is_empty() {
-        eprintln!("{}", usage_line());
+        eprintln!("{}", usage_line(name));
         return Ok(1);
     }
 
